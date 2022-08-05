8aaea2ea7ca9bc5f212499e1f0dbfb4227a6472c
wkq <1571478933@qq.com>
2022-08-05 13:43:02 +0800

    整页滑动

---
+++ app/src/main/java/com/wkq/snaphelper/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.wkq.snaphelper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.*
+import com.wkq.snaphelper.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    var binding: ActivityMainBinding? = null
+    val path = "https://img2.baidu.com/it/u=686238839,986827545&fm=253&fmt=auto&app=138&f=JPEG"
+    var stringList= arrayListOf<String>(path,path,path,path,path,path,path,path,path,path,path,path,path,path,path,path,path)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        initView()
+        startActivity()
+    }
+
+    private fun initView() {
+        //设置布局选择
+        binding!!.rvContent.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
+        //默认网格布局
+        var mAdapter = SnapAdapter(this)
+        binding!!.rvContent.adapter = mAdapter
+        mAdapter.addItems(stringList)
+
+        var pageHelp=LinearSnapHelper()
+
+        pageHelp.attachToRecyclerView( binding!!.rvContent)
+    }
+}
